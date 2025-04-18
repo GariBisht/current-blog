96d8794996573076ea7d4b5e736975b5e16938d9
Garima Bisht <74089873+GariBisht@users.noreply.github.com>
2025-04-18 21:38:25 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Blog from "./pages/Blog"; // We'll create this later
-import DarkModeToggle from "./components/DarkModeToggle";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Blog from "./pages/Blog"; // We'll create this later
+// import DarkModeToggle from "./components/DarkModeToggle";
 
-export default function App() {
-  return (
-    <Router>
-      <DarkModeToggle />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
-    </Router>
-  );
-}
+// export default function App() {
+//   return (
+//     <Router>
+//       <DarkModeToggle />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/blog" element={<Blog />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
